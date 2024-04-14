0154580dec61aa85105f15ffabc696b92794308d
Arun Dandiboina <d.arun672002@gmail.com>
2024-04-14 16:04:52 +0530

    app.js database

--- app.js
+++ app.js
@@ -33,10 +33,10 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 const db = new pg.Client({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_URL,
+  database: process.env.POSTGRES_DATABASE,
+  password: process.env.POSTGRES_PASSWORD,
   port: process.env.PG_PORT,
 });
 db.connect();
