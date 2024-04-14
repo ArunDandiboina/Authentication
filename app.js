c10bff3f94278a2c9927a3aaa5aa8624f005530a
Arun Dandiboina <d.arun672002@gmail.com>
2024-04-14 16:28:38 +0530

    URL add

--- app.js
+++ app.js
@@ -178,7 +178,7 @@ passport.use("local",
 passport.use("google",new GoogleStrategy({
   clientID : process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL:"http://localhost:3000/auth/google/secrets",
+  callbackURL:"https://authentication-iota-dun.vercel.app/auth/google/secrets",
   userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo",
 },async function(accessToken, refreshToken,profile,cb){
   try {
